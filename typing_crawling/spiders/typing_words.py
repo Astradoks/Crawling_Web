6641bbd8bd808865b1f49233a0542c1b28c85a9e
Astradoks <paul.bonilla3000@hotmail.com>
2021-08-01 20:56:40 -0500

    Crawl english words

--- typing_crawling/spiders/typing_words.py
+++ typing_crawling/spiders/typing_words.py
@@ -9,7 +9,8 @@ class TypingWordsSpider(scrapy.Spider):
 
     def parse(self, response):
         links = response.css('p > a::attr(href)').extract()
-        for link in links[:5]:
+        yield scrapy.Request(links[0], callback=self.parse_english_words)
+        for link in links:
             yield scrapy.Request(link, callback=self.parse_words)
     
     def parse_words(self, response):
@@ -18,5 +19,14 @@ class TypingWordsSpider(scrapy.Spider):
             words.append(Selector(response).xpath('..//tbody/tr['+ str(x) +']/td[2]/text()').extract_first())
         item = TypingCrawlingItem()
         item['language'] = Selector(response).xpath('..//tbody/tr[1]/td[2]/strong/text()').extract_first()
+        item['words'] = words
+        yield item
+
+    def parse_english_words(self, response):
+        words = []
+        for x in range(2,1002):
+            words.append(Selector(response).xpath('..//tbody/tr['+ str(x) +']/td[3]/text()').extract_first())
+        item = TypingCrawlingItem()
+        item['language'] = 'English'
         item['words'] = words
         yield item
